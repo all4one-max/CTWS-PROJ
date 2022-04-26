8828854741e7783d98785282694b57c341bef973
Saurav jha <jhasaurav215@gmail.com>
2022-04-26 20:00:46 +0530

    changes

--- PROJECT-5-DevOps/BDA Algo/R-ggplot.R
+++ PROJECT-5-DevOps/BDA Algo/R-ggplot.R
@@ -20,7 +20,7 @@ CO2 %>%
   ggplot(aes(conc, uptake,
          colour = Treatment)) +
   geom_point(size = 3, alpha = 0.5) +
-  geom_smooth(method = lm, se = F) + 
+  geom_smooth(method = lm, se = F) +
   facet_wrap(~Type) +
   labs(title = "conc of co2") +
   theme_bw()
