701da045a18331b44d94b7c7b10d4ae77a0b2d95
Saurav jha <jhasaurav215@gmail.com>
2022-04-27 06:57:24 +0530

    added hadoop

---
+++ PROJECT-5-DevOps/BDA Algo/hadoop/word_count/reducer.py
@@ -0,0 +1,30 @@
+import sys
+current_word = None
+current_count = 0
+word = None
+for line in sys.stdin:
+    # remove leading and trailing whitespaces
+    line = line.strip()
+    # parse the input we got from mapper.py
+    word, count = line.split('\t', 1)
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print('%s\t%s'%(current_word, current_count))
+        current_count = count
+        current_word = word
+if current_word == word:
+    print('%s\t%s'%(current_word, current_count))
+
+"""
+    Desktop would be unsder mnt/C/Users/jhasa/Desktop
+    cat input.txt | python3 mapper.py | sort | python3 reducer.py
+"""
